cf336b4582217a6da6fb1e2ad360b6304a8e41b2
darinvi <d.v.ivanov98@gmail.com>
2026-01-13 10:39:56 +0200

    wip

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
+from re import X
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 import argparse
 
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    print("Using GPU:", gpus)
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+else:
+    print("No GPU found, training will use CPU.")
+
 def main(args):
     
     data = pd.read_parquet(f"gs://{args.bucket_name}/{args.dataset_name}")
 
-    '''
-    
-    Insert Code Here.
-    
-    '''
+    ''' INSERT CUSTOM CODE HERE '''
 
+    # e.g
+    X = data.drop(columns=['y'])
+    y = data['y']
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(100, activation='relu'),
+        tf.keras.layers.Dense(1)
+    ])
+
+    model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+
+    model.fit(X, y, epochs=5)
+
+    print(model.evaluate(X, y))
+
+    ''' CUSTOM CODE ENDS HERE '''
+    
     model.save(f"gs://{args.bucket_name}/models/{args.model_name}")
 
 if __name__ == '__main__':
--- run.py
+++ run.py
@@ -1,36 +1,29 @@
-import zipfile
-import io
-import requests
 from google.cloud import aiplatform, storage
-from google.oauth2 import service_account   
+import shutil
 import tarfile
 import os
-import time
 import subprocess
 from dotenv import load_dotenv
 import argparse
-import logging
-import uuid
 
 load_dotenv()
 
-BUCKET_NAME = os.getenv('BUCKET_NAME')
 PROJECT_ID = os.getenv('PROJECT_ID')
 REGION = os.getenv('REGION')
-MACHINE_TYPE = os.getenv('MACHINE_TYPE')
 SERVICE_ACCOUNT = os.getenv('SERVICE_ACCOUNT')
+MACHINE_TYPE = 'n1-highmem-8'
 REPLICA_COUNT = 1
 
 
-def upload_to_gcs(source_file_name, destination_blob_name):
+def upload_to_gcs(bucket_name, source_file_name, destination_blob_name):
     storage_client = storage.Client()
-    bucket = storage_client.bucket(BUCKET_NAME)
+    bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
     if blob.exists():
         blob.delete()
-        logging.info(f"Deleted existing blob: {destination_blob_name}")
+        print(f"Deleted existing blob: {destination_blob_name}")
     blob.upload_from_filename(source_file_name)
-    logging.info(f"File {source_file_name} uploaded to {destination_blob_name}.")
+    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
 
 def make_tar_gz(source_dir, output_filename):
     with tarfile.open(output_filename, "w:gz") as tar:
@@ -63,65 +56,69 @@ def run_job(args):
     if args.module_name.endswith(".py"):
         args.module_name = args.module_name[:-3]
 
-    logging.info(f"Generating setup.py for {args.module_name}")
+    print(f"Generating setup.py for {args.module_name}")
     generate_setup_py(args.module_name)
-    logging.info(f"Generated setup.py for {args.module_name}")
+    print(f"Generated setup.py for {args.module_name}")
 
-    logging.info(f"Creating package")
+    print(f"Creating package")
     subprocess.run(["python", "setup.py", "sdist"], check=True)
 
-    logging.info(f"Uploading package")
-    upload_to_gcs(BUCKET_NAME, f'dist/{args.module_name}-0.1.tar.gz', f'{args.module_name}-0.1.tar.gz')
-    logging.info(f"Uploaded package")
+    print(f"Uploading package")
+    upload_to_gcs(args.bucket_name, f'dist/{args.module_name}-0.1.tar.gz', f'{args.module_name}-0.1.tar.gz')
+    print(f"Uploaded package")
 
-    logging.info(f"Uploading Dataset")
-    upload_to_gcs(BUCKET_NAME, f'dist/{args.module_name}-0.1.tar.gz', f'{args.module_name}-0.1.tar.gz')
-    logging.info(f"Uploaded Dataset")
+    shutil.rmtree("dist")
+    shutil.rmtree(f"{args.module_name}.egg-info")
+    os.remove("setup.py")
 
-    logging.info(f"Initializing Vertex AI")
+    print(f"Uploading Dataset")
+    upload_to_gcs(args.bucket_name, args.dataset_name, args.dataset_name)
+    print(f"Uploaded Dataset")
+
+    print(f"Initializing Vertex AI")
     aiplatform.init(
         project=PROJECT_ID,
         location=REGION,
-        staging_bucket=f"gs://{BUCKET_NAME}",
+        staging_bucket=f"gs://{args.bucket_name}",
     )
-    logging.info(f"Initialized Vertex AI")
+    print(f"Initialized Vertex AI")
 
-    logging.info(f"Creating job")
+    print(f"Creating job")
     job = aiplatform.CustomPythonPackageTrainingJob(
-        python_package_gcs_uri=f"gs://{BUCKET_NAME}/{args.module_name}-0.1.tar.gz",
+        python_package_gcs_uri=f"gs://{args.bucket_name}/{args.module_name}-0.1.tar.gz",
         python_module_name=args.module_name,
         display_name=f"train-{args.model_name}",
         container_uri="us-docker.pkg.dev/vertex-ai/training/tf-gpu.2-11:latest",
     )
-    logging.info(f"Created job")
+    print(f"Created job")
 
     args_list = [
-        f'--bucket-name={BUCKET_NAME}',
-        f'--dataset-name={args.dataset_name}'
+        f'--bucket-name={args.bucket_name}',
+        f'--dataset-name={args.dataset_name}',
         f'--model-name={args.model_name}',
     ]
 
-    logging.info(f"Running job")
+    print(f"Running job")
     model = job.run(
         args=args_list,
         replica_count=REPLICA_COUNT,
         machine_type=MACHINE_TYPE,
-        base_output_dir=f"gs://{BUCKET_NAME}/output/",
+        base_output_dir=f"gs://{args.bucket_name}/output/",
         service_account=SERVICE_ACCOUNT,
         accelerator_type="NVIDIA_TESLA_T4",
         accelerator_count=1,
-        sync=False
     )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model-name", type=str, required=True)
+    parser.add_argument("--bucket-name", type=str, required=True)
     parser.add_argument("--dataset-name", type=str, required=True)
-    parser.add_argument("--module-name", type=str, required=True)
+    parser.add_argument("--module-name", default='main', type=str)
     
     args = parser.parse_args()
 
-    if not args.endswith(".parquet"):
+    if not args.dataset_name.endswith(".parquet"):
         raise Exception("Expecting parquet for data")
         
     run_job(args)
